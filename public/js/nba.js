64382ef96adb1df9302f0965e1b267edd7386af2
raydphillips3 <64376825+raydphillips3@users.noreply.github.com>
2020-07-08 18:25:40 -0500

    Add files via upload

--- public/js/nba.js
+++ public/js/nba.js
@@ -1,37 +1,37 @@
 $(document).ready(() => {
-    $("#inputGroupSelectNBA").change(() => {
+  $("#inputGroupSelectNBA").change(() => {
     $("#schedule").empty();
-      const schedTeam = $("#inputGroupSelectNBA").val();
-      console.log("This is the selected team " + schedTeam);
-      getSchedules(schedTeam);
-    });
-    //==========Get Schedule========================================
-    function getSchedules(schedTeam) {
-      if (schedTeam !== "") {
-        $.ajax({
-          url: "https://api.sportsdata.io/v3/nba/scores/json/Games/2020?key=c728feb9576c4b9a8005c85667976f1e",
-          method: "GET"
-        }).then(function (resNBA) {
-          let tray = resNBA.length;
-       for (i = 0; i < tray; i++) {
-    if (schedTeam ===   resNBA[i].HomeTeam) {
-    console.log(resNBA[i].AwayTeam + " at " +   resNBA[i].HomeTeam);
-    console.log(resNBA[i].DateTime);
-    console.log(resNBA[i].Status);
-    console.log("------------------------------------------");
-    let game = resNBA[i].AwayTeam + " at " +   resNBA[i].HomeTeam;
-    let gameTime = $("<li> Time: " + resNBA[i].DateTime + "</li>");
-    let gameStatus = $("<li> Status: " + resNBA[i].Status + "</li>");
-    let gameList = $("<li> Game: " + game + "</li>");
-    $("#schedule").append(gameList);
-    $("#schedule").append(gameTime);
-    $("#schedule").append(gameStatus);
-    $("#schedule").append("------------------------------");
-    }
-    }
-          schedTeam = "";
-        })
-      }
-    }
+    const schedTeam = $("#inputGroupSelectNBA").val();
+    console.log("This is the selected team " + schedTeam);
+    getSchedules(schedTeam);
   });
-  
+  //==========Get Schedule========================================
+  function getSchedules(schedTeam) {
+    if (schedTeam !== "") {
+      $.ajax({
+        url:
+          "https://api.sportsdata.io/v3/nba/scores/json/Games/2020?key=c728feb9576c4b9a8005c85667976f1e",
+        method: "GET"
+      }).then(resNBA => {
+        const tray = resNBA.length;
+        for (i = 0; i < tray; i++) {
+          if (schedTeam === resNBA[i].HomeTeam) {
+            console.log(resNBA[i].AwayTeam + " at " + resNBA[i].HomeTeam);
+            console.log(resNBA[i].DateTime);
+            console.log(resNBA[i].Status);
+            console.log("------------------------------------------");
+            const game = resNBA[i].AwayTeam + " at " + resNBA[i].HomeTeam;
+            const gameTime = $("<li> Time: " + resNBA[i].DateTime + "</li>");
+            const gameStatus = $("<li> Status: " + resNBA[i].Status + "</li>");
+            const gameList = $("<li> Game: " + game + "</li>");
+            $("#schedule").append(gameList);
+            $("#schedule").append(gameTime);
+            $("#schedule").append(gameStatus);
+            $("#schedule").append("------------------------------");
+          }
+        }
+        schedTeam = "";
+      });
+    }
+  }
+});
--- public/js/nfl.js
+++ public/js/nfl.js
@@ -9,32 +9,29 @@ $(document).ready(() => {
   function getSchedules(schedTeam) {
     if (schedTeam !== "") {
       $.ajax({
-        url: "https://api.sportsdata.io/v3/nfl/scores/json/Schedules/2020?key=c2e06a74868d43b897386bacb10ad600",
+        url:
+          "https://api.sportsdata.io/v3/nfl/scores/json/Schedules/2020?key=c2e06a74868d43b897386bacb10ad600",
         method: "GET"
-      }).then(function (resNFL) {
-        let tray = resNFL.length;
-     for (i = 0; i < tray; i++) {
-  if (schedTeam === resNFL[i].HomeTeam) {
-  console.log(resNFL[i].AwayTeam + " at " + resNFL[i].HomeTeam);
-  console.log(resNFL[i].Date);
-  console.log(resNFL[i].Status);
-  console.log("------------------------------------------");
-  let game = resNFL[i].AwayTeam + " at " + resNFL[i].HomeTeam;
-  let gameTime = $("<li> Time: " + resNFL[i].Date + "</li>");
-  let gameStatus = $("<li> Status: " + resNFL[i].Status + "</li>");
-  let gameList = $("<li> Game: " + game + "</li>");
-  $("#schedule").append(gameList);
-  $("#schedule").append(gameTime);
-  $("#schedule").append(gameStatus);
-  $("#schedule").append("------------------------------");
-
-
-
-
-  }
+      }).then(resNFL => {
+        const tray = resNFL.length;
+        for (i = 0; i < tray; i++) {
+          if (schedTeam === resNFL[i].HomeTeam) {
+            console.log(resNFL[i].AwayTeam + " at " + resNFL[i].HomeTeam);
+            console.log(resNFL[i].Date);
+            console.log(resNFL[i].Status);
+            console.log("------------------------------------------");
+            const game = resNFL[i].AwayTeam + " at " + resNFL[i].HomeTeam;
+            const gameTime = $("<li> Time: " + resNFL[i].Date + "</li>");
+            const gameStatus = $("<li> Status: " + resNFL[i].Status + "</li>");
+            const gameList = $("<li> Game: " + game + "</li>");
+            $("#schedule").append(gameList);
+            $("#schedule").append(gameTime);
+            $("#schedule").append(gameStatus);
+            $("#schedule").append("------------------------------");
+          }
         }
         schedTeam = "";
-      })
+      });
     }
   }
 });
